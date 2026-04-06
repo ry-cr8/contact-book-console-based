cb0825487e1eb719061f5718e6158bf92e598f4d
kixxy-a11y <santos.harryjake.fillarca@gmail.com>
2026-04-06 08:50:28 +0800

    Added the update contact feature

--- contactMain.py
+++ contactMain.py
@@ -23,8 +23,8 @@ while True:
             print("Name cannot be empty!")
 
         else:
-            phone_number = input("Enter phone").strip()
-            email = input("Enter email: ").strip
+            phone_number = input("Enter phone: ").strip()
+            email = input("Enter email: ").strip()
 
             contacts = {"name" : name, "phone" : phone_number, "email" : email}
 
@@ -32,20 +32,56 @@ while True:
             print("Contact added successfully!")
 
     elif opt == 2:
+        print("All contacts")
+        for each_contact in contacts_list:
+                print(f"Name: {each_contact['name']}\nPhone number: {each_contact['phone']}\nEmail: {each_contact['email']}")
+
+
+    elif opt == 3:
         search_name = input("Enter a name to search: ").strip().lower()
 
         found = False
 
         for each_contact in contacts_list:
             if search_name in each_contact["name"].lower():
-                print(f"Contact found!\nName: {each_contact['name']}\nPhone number: {each_contact['phone']}\nEmail: {each_contact['email']}")
+                print(
+                    f"Contact found!\nName: {each_contact['name']}\nPhone number: {each_contact['phone']}\nEmail: {each_contact['email']}")
                 found = True
 
         if not found:
             print("Contact not found.")
 
-    elif opt == 3:
-        print("Update Info.")
+        elif opt == 4:
+            print("Update Contact")
+
+            search_name = input("Enter a name to update: ").strip().lower()
+
+            found = False
+            for each_contact in contacts_list:
+                if search_name in each_contact["name"].lower():
+                    each_contact["name"] = search_name
+                    found = True
+
+                    new_name = input("Enter new name (leave blank to keep current): ").strip()
+                    new_phone = input("Enter new phone (leave blank to keep current): ").strip()
+                    new_email = input("Enter new email (leave blank to keep current): ").strip()
+
+                    if new_name :
+                        new_name["phone"] = new_phone
+                    if new_phone :
+                        each_contact["phone"] = new_phone
+                    if new_email :
+                        each_contact["email"] = new_email
+                    print("Contact updated successfully!")
+                    found = True
+                    break
+
+                if not found:
+                    print("Contact not found.")
+
+        elif opt == 5:
+            print("Delete Contact")
+
 
         
 
